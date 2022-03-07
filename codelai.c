1966f7644e35b56db8da1fff1c667be34960464a
QuanTM167 <88870187+QuanTM167@users.noreply.github.com>
2022-03-07 20:36:50 +0700

    bài tập 3

---
+++ codelai.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+    printf("Data type int: %d bytes.\n", sizeof(int));
+    printf("Datatype char: %d bytes.\n", sizeof(char));
+    printf("Datatype long: %i bytes.\n", sizeof(long));
+    printf("Datatype double: %u byte.\n", sizeof(double));
+    return 0;
+}
