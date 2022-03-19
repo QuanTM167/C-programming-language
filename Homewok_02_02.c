58c55f3124afb9166c92c0ec9f3da00aac1cad86
QuanTM167 <88870187+QuanTM167@users.noreply.github.com>
2022-03-19 19:13:29 +0700

    Homewok_02_02.c

---
+++ Homewok_02_02.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    float x = sqrt(pow(3,2) + pow(4,2));
+    float y = fabs(ceil(-2.6) - floor(-2.6));
+    printf("x = %f \n y = %f", x, y);
+    return 0;
+}
