16edea95868c23529ab1eb28ae1b44603e628bff
QuanTM167 <88870187+QuanTM167@users.noreply.github.com>
2022-03-29 00:41:52 +0700

    Create Homework_04_04.c

---
+++ Homework_04_04.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char c;
+    printf("Enter the character : ");
+    scanf("%c", &c);
+    printf("The character code is : %d \n", c);
+
+    int a;
+    printf("Nhap so nguyen : ");
+    scanf("%d", &a);
+    printf("\nKi tu tuong ung la: %c", a);
+
+    return 0;
+}
