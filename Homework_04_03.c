b9329cbb6d2fc80a2544f7c174ff1f6d9e3f857e
QuanTM167 <88870187+QuanTM167@users.noreply.github.com>
2022-03-28 18:19:07 +0700

    Homework_04_03.c

---
+++ Homework_04_03.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float a, b, c, d, e, sum;
+    printf("Math scores: ");
+    scanf("%f", &a);
+    printf("Physics scores: ");
+    scanf("%f", &b);
+    printf("Chemistry scores: ");
+    scanf("%f", &c);
+    printf("Literature scores: ");
+    scanf("%f", &d);
+    printf("English scores: ");
+    scanf("%f", &e);
+    sum = (a + b + c + d + e) / 5;
+    printf("The average score of that student: %f", sum);
+}
