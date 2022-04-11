d97de6eb9aa3d7737e53165c920dcf7ed5369fcc
QuanTM167 <88870187+QuanTM167@users.noreply.github.com>
2022-04-11 14:38:38 +0700

    Update Homework_05_03.c

--- Homework_05_03.c
+++ Homework_05_03.c
@@ -1,19 +1,23 @@
 //Lập chương trình để tính các tổng sau S1 = 1+x+x^2+x^3 + ... + x^n
+// S2 = 1 - x + x^2 - x^3 + ... + (-1)^n.x^n
 #include<stdio.h>
 #include<math.h>
 int main()
 {
     int i,n;
-    float s,x;
-    s = 1;
+    float s1,s2,x;
+    s1 = 1;
+    s2 = 1;
     printf("Nhap x : ");
     scanf("%f", &x);
     printf("Nhap n : ");
     scanf("%d", &n);
     for(i=1; i<=n; i++)
     {
-        s = s + pow(x,i);
+        s1 = s1 + pow(x,i);
+        s2 = s2 + pow(-x,i);
     }
-    printf("Tong la s = %f",s);
+    printf("Tong la s1 = %f",s1);
+    printf("Tong la s2 = %f",s2);
     return 0;
 }
